9421af42b9a3941af15c5de038093bfd7142990b
Abhishek Singh <abhiwanttochat@gmail.com>
2019-07-04 07:59:31 +0530

    Bean lifecycle method annotations

--- SpringAndHibernate-luv2code/src/com/luv2code/springdemo/RandomFortuneService.java
+++ SpringAndHibernate-luv2code/src/com/luv2code/springdemo/RandomFortuneService.java
@@ -1,14 +1,24 @@
 package com.luv2code.springdemo;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Component;
 
 @Component
-public class RandomService implements FortuneService{
-
+public class RandomFortuneService implements FortuneService{
+    
+	// create an array of strings
+	private String[] data = {"Wifi is working", "There is TV in hall", "Where is the food"};
+	
+	//create a random number generator
+	private Random myRandom = new Random();
+	
+	
 	@Override
 	public String getFortune() {
-		// TODO Auto-generated method stub
-		return null;
+		int index = myRandom.nextInt(data.length);
+		String theFortune = data[index];
+		return theFortune;
 	}
 
 }
--- SpringAndHibernate-luv2code/src/com/luv2code/springdemo/TennisCoach.java
+++ SpringAndHibernate-luv2code/src/com/luv2code/springdemo/TennisCoach.java
@@ -1,5 +1,8 @@
 package com.luv2code.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -14,6 +17,18 @@ public class TennisCoach implements Coach{
 	//define a default constructor
 	public TennisCoach() {
 		System.out.println(">> Tennis Coach: inside default constructor");
+	}
+	
+	//define my init method
+	@PostConstruct
+	public void doMyStartupStuff() {
+		System.out.println(">> TennisCoach: inside of my doMyStartupStuff() method");
+	}
+	
+	//define my destroy method
+	@PreDestroy
+	public void doMyCleanupStuff() {
+		System.out.println(">> TennisCoach: inside of my doMyCleanupStuff() method");
 	}
 	
 	//non setter method
